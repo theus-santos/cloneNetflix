9cef5d17db3025f72a37b7df69b5d19c4214a6a3
Matheus Santos <Matheus_Santos@jabil.com>
2023-01-04 11:23:44 -0400

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,4 @@
+let api_key = "b1ae752a26e404c4524dc6dd91ce605c"
+let img_url = "https://image.tmdb.org/t/p/w500"
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
